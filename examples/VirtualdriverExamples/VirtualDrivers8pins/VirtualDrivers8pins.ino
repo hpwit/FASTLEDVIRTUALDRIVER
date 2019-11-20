4b5eef743680c17cc3853771b2e75fea70e404d9
YvesB <bazin.yves@gmail.com>
2019-11-20 12:31:41 +0100

    all color scheme

--- examples/VirtualdriverExamples/VirtualDrivers8pins/VirtualDrivers8pins.ino
+++ examples/VirtualdriverExamples/VirtualDrivers8pins/VirtualDrivers8pins.ino
@@ -33,6 +33,29 @@ i would advice to add :
     #define STATIC_COLOR_RGB 1      for RGB strips
 this will pre compute the color order and avoid possible hickups
 
+
+
+how to declare your strip
+	FastLED.addLeds<VIRTUAL_DRIVER,Pins,CLOCK_PIN, LATCH_PIN>(leds,NUM_LEDS_PER_STRIP); //the default order being GRB
+or
+	FastLED.addLeds<VIRTUAL_DRIVER,Pins,CLOCK_PIN, LATCH_PIN,GRB>(leds,NUM_LEDS_PER_STRIP); //to change the order works only if you do not use  #define STATIC_COLOR_XXX
+
+
+There is an option to control the COLOR order per pin (all the strips of the same pin will have to have the same color order)
+You need to add BEFORE #include "Fastled.h"
+#define STATIC_COLOR_PER_PIN 1
+#define COLOR_X_RGB 1 //this will put all the strips plugged to the X pin on RGB
+you do not need to declare if the color order is GRB
+For instance if you have 8 strips in GRB on the first declared pin and 8 strips in RGB on the second declared pin, you will write
+#define STATIC_COLOR_PER_PIN 1
+#define COLOR_2_RGB 1
+
+declare your strips as usual
+FastLED.addLeds<VIRTUAL_DRIVER,Pins,CLOCK_PIN, LATCH_PIN>(leds,NUM_LEDS_PER_STRIP);
+in that case
+	FastLED.addLeds<VIRTUAL_DRIVER,Pins,CLOCK_PIN, LATCH_PIN,GRB>(leds,NUM_LEDS_PER_STRIP); will have no effect in the order color
+
+
 */
 #include "FastLED.h"
 #define LATCH_PIN 13
